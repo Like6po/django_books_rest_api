2ddca42fa5eaac98784984721146a2aeea57de1f
Like6po <begemot122122@gmail.com>
2022-08-23 11:37:52 +0300

    🚀: Доделал view книг

--- books_app/api/v1/serializers/book.py
+++ books_app/api/v1/serializers/book.py
@@ -1,5 +1,6 @@
-from rest_framework import serializers
+from rest_framework import serializers, status
 
+from api.v1.models.author import Author
 from api.v1.models.book import Book
 from api.v1.serializers.author import AuthorSerializer
 
@@ -20,17 +21,40 @@ class BooksSerializer(serializers.Serializer):
         return new_book
 
 
+class BookCommentSerializer(serializers.Serializer):
+    id = serializers.IntegerField(required=False)
+    created_at = serializers.DateTimeField(required=False)
+    author = AuthorSerializer(required=False, read_only=True)
+    text = serializers.CharField(max_length=4096)
+
+
 class BookSerializer(serializers.Serializer):
     id = serializers.IntegerField(required=False)
     created_at = serializers.DateTimeField(required=False)
-    name = serializers.CharField(max_length=256)
-    publish_date = serializers.DateField("%d.%m.%Y")
-    archived = serializers.BooleanField()
+    name = serializers.CharField(max_length=256, required=False)
+    publish_date = serializers.DateField("%d.%m.%Y", required=False)
+    archived = serializers.BooleanField(required=False)
     authors = AuthorSerializer(many=True, required=False, read_only=True)
+    comment_set = BookCommentSerializer(many=True, required=False, read_only=True)
+
+
+class BookUpdateSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    created_at = serializers.DateTimeField(read_only=True)
+    name = serializers.CharField(max_length=256, required=False)
+    publish_date = serializers.DateField("%d.%m.%Y", required=False)
+    archived = serializers.BooleanField(required=False)
+    authors = serializers.PrimaryKeyRelatedField(many=True, required=False, queryset=Author.objects.all())
+
+    def validate(self, data):
+        if not data:
+            raise serializers.ValidationError("Empty data", code=status.HTTP_400_BAD_REQUEST)
+        return data
 
     def update(self, instance: Book, validated_data):
         instance.name = validated_data.get("name", instance.name)
         instance.publish_date = validated_data.get("publish_date", instance.publish_date)
         instance.archived = validated_data.get("archived", instance.archived)
+        instance.authors.set(validated_data.get("authors", instance.authors.all()))
         instance.save()
         return instance
--- books_app/api/v1/views/book.py
+++ books_app/api/v1/views/book.py
@@ -1,13 +1,11 @@
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from api.v1.models.book import Book
-from api.v1.models.comment import Comment
-from api.v1.permissions import IsOwnerOrReadOnly
-from api.v1.serializers.book import BooksSerializer, BookSerializer
+from api.v1.serializers.book import BooksSerializer, BookSerializer, BookUpdateSerializer
 
 
 class BooksView(APIView):
@@ -28,22 +26,15 @@ class BooksView(APIView):
 
 class BookView(APIView):
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get(self, request: Request, book_id: int):
         try:
             book = Book.objects.get(id=book_id)
         except Book.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        comments = Comment.objects.filter(book=book.id)
-        return Response({"id": book.id,
-                         "name": book.name,
-                         "publish_date": book.publish_date,
-                         "comments": [{"id": comment.id,
-                                       "text": comment.text,
-                                       "author": {"id": comment.author.id,
-                                                  "full_name": comment.author.full_name()}}
-                                      for comment in comments.all()]})
+        serializer = BookSerializer(book, context={'request': request})
+        return Response(serializer.data)
 
     def delete(self, request: Request, book_id: int):
         try:
@@ -53,13 +44,12 @@ class BookView(APIView):
         book.delete()
         return Response(status=status.HTTP_200_OK)
 
-    def put(self, request: Request, book_id):
+    def patch(self, request: Request, book_id):
         try:
             book = Book.objects.get(id=book_id)
         except Book.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        serializer = BookSerializer(data=request.data, instance=book)
-        if not serializer.is_valid():
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+        serializer = BookUpdateSerializer(data=request.data, instance=book)
+        serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
