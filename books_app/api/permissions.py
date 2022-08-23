c9d0f8ad470ed93c8643d31d416d0c2d93901cc7
Like6po <begemot122122@gmail.com>
2022-08-23 15:29:09 +0300

    🚀: Вернул файл к прошлому состоянию

--- books_app/api/permissions.py
+++ books_app/api/permissions.py
@@ -5,11 +5,7 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        # print(obj.author == request.user)
         return obj.author == request.user
-
-    def has_permission(self, request, view):
-        return self.has_object_permission(request, view)
 
 
 class IsSelfOrReadOnly(permissions.BasePermission):
