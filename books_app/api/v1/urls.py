505a45a9785b765249402e68f8ef3f8d9dc4cfc6
Like6po <begemot122122@gmail.com>
2022-08-30 10:41:55 +0300

    🚀: Добавил новые url

--- books_app/api/v1/urls.py
+++ books_app/api/v1/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import book, author, auth, comment
+from .views import author, auth
+from .views.book import rating, book, comment, favorite
 
 urlpatterns = [
+    # Auth
     path('register-author', auth.RegisterAuthorView.as_view()),
     path('register-user', auth.RegisterUserView.as_view()),
     path('confirm/<str:code>', auth.ConfirmRegisterView.as_view()),
@@ -10,10 +12,19 @@ urlpatterns = [
     path('refresh', auth.RefreshView.as_view()),
     path('recovery', auth.RecoveryUserView.as_view()),
     path('recovery/<str:code>', auth.RecoveryUserChangePasswordView.as_view()),
+
+    # Authors
     path('authors', author.AuthorsView.as_view()),
     path('authors/<int:author_id>', author.AuthorView.as_view()),
+
+    # Books
     path('books', book.BooksView.as_view()),
     path('books/<int:book_id>', book.BookView.as_view()),
     path('books/<int:book_id>/comments', comment.CommentsView.as_view()),
-    path('books/<int:book_id>/comments/<int:comment_id>', comment.CommentView.as_view())
+    path('books/<int:book_id>/comments/<int:comment_id>', comment.CommentView.as_view()),
+    path('books/<int:book_id>/ratings', rating.RatingsView.as_view()),
+    path('books/<int:book_id>/ratings/<int:rating_id>', rating.RatingView.as_view()),
+
+    # Favorite
+    path('books/favorite', favorite.RatingsView.as_view())
 ]
