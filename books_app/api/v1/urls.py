1330a15acba8e5ed9789ad7c28aa285fb052167e
Like6po <begemot122122@gmail.com>
2022-08-30 17:33:24 +0300

    🚀: Имена для url

--- books_app/api/v1/urls.py
+++ books_app/api/v1/urls.py
@@ -5,26 +5,26 @@ from .views.book import rating, book, comment, favorite
 
 urlpatterns = [
     # Auth
-    path('register-author', auth.RegisterAuthorView.as_view()),
-    path('register-user', auth.RegisterUserView.as_view()),
-    path('confirm/<str:code>', auth.ConfirmRegisterView.as_view()),
-    path('login', auth.LoginView.as_view()),
-    path('refresh', auth.RefreshView.as_view()),
-    path('recovery', auth.RecoveryUserView.as_view()),
-    path('recovery/<str:code>', auth.RecoveryUserChangePasswordView.as_view()),
+    path('register-author', auth.RegisterAuthorView.as_view(), name="register_author"),
+    path('register-user', auth.RegisterUserView.as_view(), name="register_user"),
+    path('confirm/<str:code>', auth.ConfirmRegisterView.as_view(), name="confirm"),
+    path('login', auth.LoginView.as_view(), name="login"),
+    path('refresh', auth.RefreshView.as_view(), name="refresh"),
+    path('recovery', auth.RecoveryUserView.as_view(), name="recovery"),
+    path('recovery/<str:code>', auth.RecoveryUserChangePasswordView.as_view(), name="recovery_code"),
 
     # Authors
-    path('authors', author.AuthorsView.as_view()),
-    path('authors/<int:author_id>', author.AuthorView.as_view()),
+    path('authors', author.AuthorsView.as_view(), name="authors"),
+    path('authors/<int:author_id>', author.AuthorView.as_view(), name="authors_detail"),
 
     # Books
-    path('books', book.BooksView.as_view()),
-    path('books/<int:book_id>', book.BookView.as_view()),
-    path('books/<int:book_id>/comments', comment.CommentsView.as_view()),
-    path('books/<int:book_id>/comments/<int:comment_id>', comment.CommentView.as_view()),
-    path('books/<int:book_id>/ratings', rating.RatingsView.as_view()),
-    path('books/<int:book_id>/ratings/<int:rating_id>', rating.RatingView.as_view()),
+    path('books', book.BooksView.as_view(), name="books"),
+    path('books/<int:book_id>', book.BookView.as_view(), name="books_detail"),
+    path('books/<int:book_id>/comments', comment.CommentsView.as_view(), name="books_comments"),
+    path('books/<int:book_id>/comments/<int:comment_id>', comment.CommentView.as_view(), name="books_comments_detail"),
+    path('books/<int:book_id>/ratings', rating.RatingsView.as_view(), name="books_ratings"),
+    path('books/<int:book_id>/ratings/<int:rating_id>', rating.RatingView.as_view(), name="books_ratings_detail"),
 
     # Favorite
-    path('books/favorite', favorite.RatingsView.as_view())
+    path('books/favorites', favorite.RatingsView.as_view(), name="favorites")
 ]
