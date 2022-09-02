ce5061db0ece74e6e66ba6ed6840745ae55c1404
Like6po <begemot122122@gmail.com>
2022-09-02 15:40:29 +0300

    🚀: Подготовка к деплою

--- books_app/enviroment/prod/settings.py
+++ books_app/enviroment/prod/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s&3co7_)x^b0%aq%%qvc&!a-n1mrk%v0j9!wvwh9f+!i@s#7nb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -154,7 +155,7 @@ DJANGO_SETTINGS_MODULE = "books_app.settings"
 
 SWAGGER_SETTINGS = {
     'SECURITY_DEFINITIONS': {
-        'api_key': {
+        'Bearer': {
             'type': 'apiKey',
             'in': 'header',
             'name': 'Authorization'
